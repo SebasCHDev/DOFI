998d43d6ccee287bfc9a82f34e1680193d9f4b7f
Sebastian Calle Herrera <tatancalle1@gmail.com>
2024-10-28 23:53:40 -0500

    Cambios generales 28 oct
    Se organizan las imágenes del slider, galería, se implementa sección quiénes somos, se establecen nuevos colores y estilos.

--- assets/JS/gallery.js
+++ assets/JS/gallery.js
@@ -1,23 +1,26 @@
-let zoom = document.querySelector(".zoom");
-let imgNormal = document.querySelectorAll(".imgNormal");
-let imgGrande = document.getElementById("imgGrande");
-let cerrarBtn = document.querySelector(".cerrar");
-let simboloZoom = document.querySelectorAll(".simboloZoom");
+// Selecciona el modal y sus elementos
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modal-image");
+const images = document.querySelectorAll(".gallery-image");
+const closeModal = document.querySelector(".close");
 
-simboloZoom.forEach(function(icon, index){
-    icon.onclick = function (){
-        let img = imgNormal[index];
-        zoom.style.display = "flex";
-        imgGrande.src = img.src;
-    }
-})
+// Abre el modal cuando se hace clic en una imagen
+images.forEach(image => {
+  image.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImage.src = image.src;
+  });
+});
 
-cerrarBtn.onclick = function(){
-    zoom.style.display = "none";
-}
+// Cierra el modal al hacer clic en la 'X'
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-window.onclick = (event) => {
-    if (event.target == zoom) {
-        zoom.style.display = "none";
-    }
-}
+// Cierra el modal al hacer clic fuera de la imagen
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
